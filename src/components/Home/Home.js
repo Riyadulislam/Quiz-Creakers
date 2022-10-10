52110bd60060f721ca78d46d71a2f5264a054cce
Riyadulislam <riyadulislambd02@gmail.com>
2022-10-11 04:51:20 +0600

    push

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    const background='https://www.learningrevolution.net/wp-content/uploads/2017/06/online-course-failure-47805990_m.jpg'
+    return (
+        <div>
+          
+          <div
+             class="bg_image"
+            >
+                <h1>This is online Quiz Course.Through This Course You Justify Your Knowledge</h1>
+          </div>
+   
+        </div>
+    );
+};
+
+export default Home;
